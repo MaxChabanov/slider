0d19fba3397e3accd4cb61a97951bd71d329ad70
Max <maxchabanov2009@gmail.com>
2021-08-05 15:24:30 +0300

    fix returned

--- app.js
+++ app.js
@@ -216,3 +216,21 @@ dotPaginationItemsArray.forEach((element) => {
     playSlideAnimation(event);
   };
 });
+
+// Swipe effect
+let startingX, startingY, movingX, movingY;
+function touchStart(evt) {
+  startingX = evt.touches[0].clientX;
+  startingY = evt.touches[0].clientY;
+}
+function touchMove(evt) {
+  movingX = evt.touches[0].clientX;
+  movingY = evt.touches[0].clientY;
+}
+function touchEnd() {
+  if (startingX + 100 < movingX) {
+    previousSlide(event);
+  } else if (startingX - 100 > movingX) {
+    nextSlide(event);
+  }
+}
